1427835e1cd8b9bc5c66e801c47028b73f7bd318
Rachael Cole <jcrachael@gmail.com>
2022-10-20 22:52:46 +1100

    CSS shading

--- scripts.js
+++ scripts.js
@@ -13,13 +13,15 @@ class Calculator {
         this.operation = undefined;
     };
 
+    // add number to current string
     appendNumber(number) {
-        if (number === '.' && this.currentOperand.includes('.')) {
-            return 
-        };
-        this.currentOperand = this.currentOperand.toString() + number.toString();
+        if (number === '.' && this.currentOperand.includes('.')) { return 
+        } else if (this.currentOperand != null && this.previousOperand != null) {
+            this.currentOperand = this.currentOperand.toString() + number.toString();
+        } else { this.currentOperand = number.toString();};
     };
 
+    // call the computation
     chooseOperation(operation) {
         if (this.currentOperand === '') return;
         if (this.previousOperand !== '') {
@@ -30,7 +32,7 @@ class Calculator {
         this.currentOperand = '';
     };
 
-    // define Operator function
+    // define Operator functions
     compute() {
         let computation;
         const prev = parseFloat(this.previousOperand);
@@ -48,7 +50,11 @@ class Calculator {
                 computation = prev * current;
                 break;
             case '÷':
-                computation = prev / current;
+                if (current === 0) { 
+                    computation = 'Error';
+                } else {
+                    computation = prev / current;
+                }
                 break;
             case '^':
                 computation = prev**current;
@@ -64,88 +70,61 @@ class Calculator {
                 break;
             default:
                 return;
-        }
+        };
         this.previousOperand = '';
         this.operation = undefined;
-        
         let stringComp = computation.toString();
-        if (stringComp.includes('.')) {
-            this.currentOperand = computation.toFixed(2);
-        } else {
-            this.currentOperand = computation;
-        }
+        if (stringComp.includes('.')) { this.currentOperand = computation.toFixed(2); } else {
+            let log = document.querySelector('[data-current-operand]');
+            if (computation === 'Error') { this.currentOperandText.innerText === 'Error'; log.innerHTML = 'Error';} 
+            else if (computation != 'Error') { this.currentOperand = computation;};
+        };
     };
 
+    // get number for display
     getDisplayNumber(number) {
         const stringNumber = number.toString();
         const integerDigits = parseFloat(stringNumber.split('.')[0]);
         const decimalDigits = stringNumber.split('.')[1];
         let integerDisplay;
-        if (isNaN(integerDigits)) {
-            integerDisplay = '';
-        } else {
-            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0});
-        }
-        if (decimalDigits != null) {
-            return `${integerDisplay}.${decimalDigits}`;
-        } else {
-            return integerDisplay;
-        }
+        if (isNaN(integerDigits)) { integerDisplay = ''; } else { 
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0});};
+        if (decimalDigits != null) { return `${integerDisplay}.${decimalDigits}`;} else { return integerDisplay;};
     };
     
+    // refresh the screen display
     updateDisplay() {
-        this.currentOperandText.innerText = this.getDisplayNumber(this.currentOperand);
-        if (this.operation != null) {
-            this.prevOperandText.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        if (this.currentOperandText.innerText === 'Error') {
+            this.prevOperandText.innerText = '';
+            this.currentOperandText.innerText = "ERROR";
         } else {
-            this.prevOperandText.innerText = this.previousOperand;
+            this.currentOperandText.innerText = this.getDisplayNumber(this.currentOperand);
+            if (this.operation != null) { this.prevOperandText.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`} 
+            else {this.prevOperandText.innerText = this.previousOperand;};
         };
     };   
 };
 
-
-
 // DOMContentLoaded
 document.addEventListener('DOMContentLoaded', (event) => {
-    // track variables
     const numberButtons = document.querySelectorAll('[data-number]');
     const operationButtons = document.querySelectorAll('[data-operation]');
     const equalsButton = document.querySelector('[data-equals]');
     const clearButton = document.querySelector('[data-clear]');
     const prevOperandText = document.querySelector('[data-previous-operand]');
     const currentOperandText = document.querySelector('[data-current-operand]');
-
     const calculator = new Calculator(prevOperandText, currentOperandText);
-    
-
-    // add event listeners to each button
+ 
     numberButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            calculator.appendNumber(button.innerText);
-            calculator.updateDisplay();
-        })
-    });
+        button.addEventListener('click', () => {calculator.appendNumber(button.innerText); calculator.updateDisplay();});});
 
     operationButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            calculator.chooseOperation(button.innerText);
-            calculator.updateDisplay();
-        })
-    });
+        button.addEventListener('click', () => {calculator.chooseOperation(button.innerText); calculator.updateDisplay();});});
 
-    equalsButton.addEventListener('click', button => {
-        calculator.compute();
-        calculator.updateDisplay();
-    });
+    equalsButton.addEventListener('click', () => {calculator.compute(); calculator.updateDisplay();});
 
-    clearButton.addEventListener('click', button => {
-        calculator.clear();
-        calculator.updateDisplay();
-    });
-
-
-    
-  });
+    clearButton.addEventListener('click', () => {calculator.clear();calculator.updateDisplay();});
+});
  
 
 
